d9371dca46102c999d6a367444c744000c291f93
yoonsangpark <yoonsangpark@gmail.com>
2025-08-19 23:34:43 +0900

    4. useStage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function Header(props){
   console.log("props", props, props.title)
@@ -34,10 +35,10 @@ function Nav(props){
   )
 }
 
-function Article(){
+function Article(props){
   return (
     <div>
-      <h2>Welcome </h2>
+      <h2> {props.title} </h2>
       Hello, WEB
     </div>
   )
@@ -45,21 +46,36 @@ function Article(){
 
 function App() {
 
+  //const _mode = useState('WELCOME');
+  //const mode = _mode[0];
+  //const setMode = _mode[1];
+  const [mode, setMode] = useState('WELCOME')
+
+
   const topics = [
     {id:1, title:'html', body:'html is...'},
     {id:2, title:'css', body:'css is...'},
     {id:3, title:'javascript', javascript:'html is...'}
   ]
 
+  let content = null;
+  if( mode === 'WELCOME') {
+    content = <Article title='Welcome'></Article>
+  } else if(mode ==='READ') {
+    content = <Article title='Read'></Article>
+  }
+
   return (
     <div className="App">
       <Header title="React" onChangeMode={()=>{
-        alert('Header');
+        //alert('Header');
+        setMode('WELCOME')
       }}></Header>
       <Nav topics={topics} onChangeMode={(id)=> {
-        alert(id);
+        //alert(id);
+        setMode('READ')
       }}></Nav>
-      <Article></Article>
+      {content}
     </div>
   );
 }
