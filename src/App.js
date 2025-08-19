85b945bd1993e19625b70db11e81bdbe8138a8bb
yoonsangpark <yoonsangpark@gmail.com>
2025-08-19 22:05:25 +0900

    1. component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(){
+  return (
+    <div>
+      <h1><a href="/"> WEB </a></h1>
+    </div>
+  )
+}
+
+function Nav(){
+  return(
+    <nav>
+      <li><a href="/read/1"> html </a></li>
+      <li><a href="/read/2"> css </a></li>
+      <li><a href="/read/3"> js </a></li>
+    </nav>
+  )
+}
+
+function Article(){
+  return (
+    <div>
+      <h2>Welcome </h2>
+      Hello, WEB
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
